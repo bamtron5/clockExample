3445a1366962d91b0aa883dc7cd4e20c8e1a9d6b
bamtron5 <brandon@brandonam.com>
2017-03-30 10:56:27 -0700

    roman numerals up to 59

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,40 @@
-setInterval(function () {
-    isTenSeconds(moment());
-}, 1000);
-function isTenSeconds(time) {
-    var curTime = time.seconds();
-    if (curTime === 10) {
-        document.getElementById('alarm').innerHTML
-            = "<h1>Wake up, it's " + curTime + " seconds</h1>";
+var clock;
+(function (clock) {
+    var output = '';
+    var firstRun = true;
+    var romanNumerals = [
+        '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
+        'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX',
+        'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX',
+        'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL',
+        'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L',
+        'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LIX'
+    ];
+    setInterval(function () {
+        isTenSeconds(moment());
+        everySecond(moment());
+    }, 1000);
+    function everySecond(time) {
+        var seconds = time.seconds();
+        console.log(roman(seconds));
     }
-}
-;
+    function isTenSeconds(time) {
+        var seconds = time.seconds();
+        if (firstRun) {
+            firstRun = false;
+            output = Array(Math.floor((seconds / 10) + 1)).join('x');
+        }
+        if (seconds % 10 === 0 && seconds !== 0) {
+            output = Array((seconds / 10) + 1).join('x');
+        }
+        else if (seconds === 0) {
+            output = '';
+        }
+        console.log("Seconds " + seconds + ": " + output);
+    }
+    ;
+    function roman(second) {
+        return romanNumerals[second];
+    }
+})(clock || (clock = {}));
 //# sourceMappingURL=script.js.map
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,14 +1,44 @@
 // import * as moment from 'moment';
-/// <reference path="../node_modules/moment/moment.d.ts" />
+declare var moment: any;
+namespace clock {
+  let output = '';
+  let firstRun = true;
+  const romanNumerals = [
+    '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
+    'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX',
+    'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX',
+    'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL',
+    'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L',
+    'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LIX'
+  ];
 
-setInterval(() => {
-  isTenSeconds(moment());
-}, 1000);
+  setInterval(() => {
+    isTenSeconds(moment());
+    everySecond(moment());
+  }, 1000);
 
-function isTenSeconds(time) {
-  let curTime = time.seconds();
-  if (curTime === 10) {
-    document.getElementById('alarm').innerHTML
-      = `<h1>Wake up, it's ${curTime} seconds</h1>`;
+  function everySecond(time) {
+    let seconds = time.seconds();
+    console.log(roman(seconds));
   }
-};
+
+  function isTenSeconds(time) {
+    let seconds = time.seconds();
+
+    if (firstRun) {
+      firstRun = false;
+      output = Array(Math.floor((seconds / 10) + 1)).join('x');
+    }
+
+    if (seconds % 10 === 0 && seconds !== 0) {
+      output = Array((seconds / 10) + 1).join('x');
+    } else if (seconds === 0) {
+      output = '';
+    }
+    console.log(`Seconds ${seconds}: ${output}`);
+  };
+
+  function roman(second) {
+    return romanNumerals[second];
+  }
+}
